faa513e071c768ccccede05f1d019cfc19f0c4fb
DannyArevaloS <darevalo_dev@outlook.es>
2024-09-29 22:47:40 +0200

    commit 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("presiona cualquier tecla para finalizar");
+        Console.ReadKey();
+    }
+}
